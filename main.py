47696889a840c90cb88952815ed5a918262c9acd
MrNightRaven <uknownhacker234@gmail.com>
2022-01-05 18:35:05 +0800

    new

---
+++ main.py
@@ -0,0 +1,28 @@
+from datetime import datetime
+from datetime import date
+
+import time, os, sys, pytz, random
+
+def getCurrentTime():
+    today = date.today()
+    tanggal = today.strftime("%d/%m%Y")
+    tz_id = pytz.timezone('Asia/Makassar')
+    datetime_MK = datetime.now(tz_ID)
+    waktu = datetime_MK.strftime("%H:%M:%S")
+    result = tanggal + " " + waktu
+    return result
+
+def predict():
+    file = open('./data.txt', 'w+')
+    prediction = random.randrange(20, 1024)
+    file.write("time prediction : %s" % (prediction))
+    file.close()
+
+def main():
+    os.system('git commit -a -m "%s"' % (getCurrentTime()))
+    os.system("git push -u origin master")
+
+while (True):
+    # main()
+    predict()
+    time.sleep(2)
